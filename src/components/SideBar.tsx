8241d583646bb8f799eaa93e661fe4029f6d60bc
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-10-18 23:12:45 -0500

    Leer campos de formularios con formik

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,8 @@
-import { Navbar, Avatar } from '@mantine/core';
+import { Navbar, Avatar, Alert, Notification } from '@mantine/core';
 import React from 'react';
 import styles from '../styles/SideBar.module.css';
 import Link from 'next/link';
+
 import {
   InfoCircle,
   Speakerphone,
@@ -12,13 +13,66 @@ import {
 // import { Modal } from './Modal';
 import { useState } from 'react';
 
-import { Modal, TextInput, Button } from '@mantine/core';
+import { Modal, Button } from '@mantine/core';
+import {
+  Form,
+  Formik,
+  FormikHelpers,
+  Field,
+  ErrorMessage,
+  FormikErrors,
+  FormikValues,
+} from 'formik';
+
+interface Values {
+  sn: string;
+  nombre: string;
+  descripcion: string;
+  unidadMedida: string;
+  latitud: string;
+  longitud: string;
+}
 interface SideBarProps {
   opened: boolean;
 }
 
 export const SideBar: React.FC<SideBarProps> = ({ opened }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [alerta, setAlerta] = useState(false);
+  const onFormSubmit = (
+    values: Values,
+    { setSubmitting, resetForm }: FormikHelpers<Values>,
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+    console.log(values);
+
+    resetForm();
+    setOpenModal(false);
+    setAlerta(true);
+  };
+  const onValidateForm = (values: Values) => {
+    const errors: FormikErrors<FormikValues> = {};
+    if (
+      !values.sn ||
+      !values.nombre ||
+      !values.descripcion ||
+      !values.unidadMedida ||
+      !values.latitud ||
+      !values.longitud
+    ) {
+      errors.sn = 'Campo Requerido';
+      errors.nombre = 'Campo Requerido';
+      errors.descripcion = 'Campo Requerido';
+      errors.unidadMedida = 'Campo Requerido';
+      errors.latitud = 'Campo Requerido';
+      errors.longitud = 'Campo Requerido';
+    }
+
+    return errors;
+  };
   return (
     <Navbar
       p="md"
@@ -76,55 +130,121 @@ export const SideBar: React.FC<SideBarProps> = ({ opened }) => {
         </div>
         <div
           onClick={() => {
-            console.log('Me dieron clic, deberia abrir el modal');
             setOpenModal(true);
             // <Modal openModal={true} setOpenModal={setOpenModal} />;
           }}
         >
           <p>Agregar Sensor</p>
         </div>
-        <Modal
-          opened={openModal}
-          onClose={() => setOpenModal(false)}
-          title="Ingresar nuevo Sensor"
-          size="70%"
+        <Formik
+          initialValues={{
+            sn: '',
+            nombre: '',
+            descripcion: '',
+            unidadMedida: '',
+            latitud: '',
+            longitud: '',
+          }}
+          onSubmit={onFormSubmit}
+          validate={onValidateForm}
         >
-          {/* Modal content */}
-          <TextInput
-            className={styles['text-item']}
-            label="SN"
-            placeholder="SN"
-          />
-          <TextInput
-            className={styles['text-item']}
-            label="Nombre"
-            placeholder="Nombre"
-          />
-          <TextInput
-            className={styles['text-item']}
-            label="Descripción"
-            placeholder="Descripción"
-          />
-          <TextInput
-            className={styles['text-item']}
-            label="Unidad de Medida"
-            placeholder="Unidad de Medida"
-          />
-          <TextInput
-            className={styles['text-item']}
-            label="Latitud"
-            placeholder="Latitud"
-          />
-          <TextInput
-            className={styles['text-item']}
-            label="Longitud"
-            placeholder="Longitud"
-          />
-          <Button color="cyan" style={{ marginTop: '20px' }}>
-            Guardar
-          </Button>
-          {/* Modal content */}
+          <Modal
+            opened={openModal}
+            onClose={() => setOpenModal(false)}
+            title="Ingresar nuevo Sensor"
+            size="70%"
+          >
+            {/* Modal content */}
+            <Form>
+              <div className={styles['input-form']}>
+                <label htmlFor="sensor">Sensor </label>
+                <Field id="sn" name="sn" placeholder="SN" />
+                <ErrorMessage
+                  name="sn"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <div className={styles['input-form']}>
+                <label htmlFor="nombre">Nombre </label>
+                <Field id="nombre" name="nombre" placeholder="Nombre" />
+                <ErrorMessage
+                  name="nombre"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <div className={styles['input-form']}>
+                <label htmlFor="descripcion">Descripción</label>
+                <Field
+                  id="descripcion"
+                  name="descripcion"
+                  placeholder="Descripción"
+                />
+                <ErrorMessage
+                  name="descripcion"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <div className={styles['input-form']}>
+                <label htmlFor="descripcion">Unidad de Medida </label>
+                <Field
+                  id="unidadMedida"
+                  name="unidadMedida"
+                  placeholder="Unidad de Medida"
+                />
+                <ErrorMessage
+                  name="unidadMedida"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <div className={styles['input-form']}>
+                <label htmlFor="latitud">Latitud</label>
+                <Field id="latitud" name="latitud" placeholder="Latitud" />
+                <ErrorMessage
+                  name="latitud"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <div className={styles['input-form']}>
+                <label htmlFor="longitud">longitud</label>
+                <Field id="longitud" name="longitud" placeholder="Longitud" />
+                <ErrorMessage
+                  name="longitud"
+                  component="div"
+                  className={styles['error-form']}
+                />
+              </div>
+
+              <Button color="cyan" style={{ marginTop: '20px' }} type="submit">
+                Guardar
+              </Button>
+            </Form>
+            {/* Modal content */}
+          </Modal>
+        </Formik>
+        {/* alerta */}
+        <Modal
+          withCloseButton={false}
+          opened={alerta}
+          onClose={() => setAlerta(false)}
+        >
+          Sensor agregado correctamente!!
+          {/* <Alert title="Bummer!" color="teal">
+            Something terrible happened! You made a mistake and there is no
+            going back, your data was lost forever!
+          </Alert> */}
         </Modal>
+
+        {/* alerta */}
       </div>
       <div className={styles['nav-container']}>
         <div>
